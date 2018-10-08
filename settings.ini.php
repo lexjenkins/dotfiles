0a284b4b3c39ffbf2fbb4cd6a37f1a71613f4a60
lexjenkins <lexjenkins@gmail.com>
2018-10-08 16:43:22 -0500

    Create settings.ini.php

---
+++ settings.ini.php
@@ -0,0 +1,73 @@
+; <?php die('Access denied'); ?>
+[general]
+title = "Muximux "
+branch = "master"
+theme = "classic"
+color = "#31ac63"
+tabcolor = "true"
+rssUrl = "http://feeds.arstechnica.com/arstechnica/index"
+userNameInput = "admin"
+password = "muximux"
+
+[settings]
+branch_changed = ""
+sha = "66b11488206b44364b6ed8bb438462cbbb0835c5"
+enabled = "true"
+default = "false"
+name = "Settings"
+url = "muximux.php"
+landingpage = "false"
+icon = "fa-cog"
+dd = "true"
+branches[develop] = "92b392c88b24faa6dd63c9ada2ee7db6b667e123"
+branches[experimental] = "262b1a4c4ca6fa0386e46400211604d66c0bd22d"
+branches[master] = "66b11488206b44364b6ed8bb438462cbbb0835c5"
+branches[parseError] = "14ea3581389e329e27c496367b2b85b15fc106f9"
+branches[saveProblem] = "4d16f3f4b1d7fb18bac5d8ed09b9a45dbe6711ff"
+last_check = 1539034781
+
+[Plex]
+name = "Plex"
+url = "http://192.168.0.23:32400/web/index.html"
+scale = 1
+icon = "muximux-plex"
+color = "#f9be03"
+enabled = "true"
+landingpage = "true"
+default = "true"
+
+[SABnzbd]
+name = "Krusader"
+url = "http://192.168.0.23:6080/vnc.html?resize=remote&host=192.168.0.23&port=6080&autoconnect=1"
+scale = 1
+icon = "muximux-list2"
+color = "#f5b907"
+enabled = "true"
+landingpage = "true"
+
+[pydio]
+name = "Tautulli"
+url = "http://192.168.0.23:8181/home"
+scale = 1
+icon = "muximux-eye"
+color = "#cc6156"
+enabled = "true"
+landingpage = "true"
+
+[Headphones]
+name = "Comics"
+url = "http://192.168.0.23:2202/ubooquity/comics/1/"
+scale = 1
+icon = "muximux-image"
+color = "#000000"
+enabled = "true"
+landingpage = "true"
+
+[deluge]
+name = "Deluge"
+url = "http://192.168.0.23:8112/"
+scale = 1
+icon = "muximux-deluge"
+color = "#304663"
+enabled = "true"
+landingpage = "true"
